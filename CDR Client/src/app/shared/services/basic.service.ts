751ea893a9a4959c9e567f83c4dc3d4165cab5ab
RahmatAliMalik5 <31620469+RahmatAliMalik5@users.noreply.github.com>
2019-11-14 16:11:10 +0500

    Update basic.service.ts

--- CDR Client/src/app/shared/services/basic.service.ts
+++ CDR Client/src/app/shared/services/basic.service.ts
@@ -13,11 +13,41 @@ export class BasicService {
 
   constructor() { }
 
+  // changeTitle(_title): void {
+  //   // console.log('=====>>>> from base service changeTitle()');
+  //   if (_title) {
+  //     this.titleSubject.next(_title);
+  //   }
+  // }
+  
+  // gets whether a http or https protocol used
+  private websiteProtol = window.location.href.split(':')[0];
+  private path = this.websiteProtol + '://cdr.isols.tech';
+  // private path = 'http://graph.themeparkview.com:3000';
 
   getPath() {
-    if (environment.production) {
-      return window.location.origin;
+    // console.log('production is on ==> ' + environment.production );
+    // if (environment.production) {
+    //   return this.path;
+    // }
+    // return 'http://localhost:3000';
+
+      // console.log(window.location);
+      const hostName = window.location.hostname;
+
+      // console.log(window.location);
+      if (hostName != 'localhost') {
+        return this.path;
+      }
+
+      // console.log('http://localhost:3000');
+
+      return 'http://localhost:3000';
     }
-    return 'http://localhost:3000';
-  }
+  // getPath() {
+  //   if (environment.production) {
+  //     return this.path;
+  //   }
+  //   return 'http://localhost:3000';
+  // }
 }
